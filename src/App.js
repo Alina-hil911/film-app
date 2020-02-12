75fc998e363a314993ee162408a4d1a6c3e78a79
Alina <asasabahali@gmail.com>
2020-02-12 02:29:44 +0200

    initial commit, added lazy loaded homepage header footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from "react";
 
+//import of components
+const Homepage = lazy(() => import("./Homepage"));
+const Header = lazy(() => import("./Header"));
+const Footer = lazy(() => import("./Footer"));
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Header></Header>
+        <Homepage></Homepage>
+        <Footer></Footer>
+      </Suspense>
     </div>
   );
 }
